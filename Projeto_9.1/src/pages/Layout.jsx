56b8cc4f3c743d616e7405b5e5a6a5cc131387e5
Gustavo.D <duavo06@gmail.com>
2024-10-21 10:08:10 -0300

    Atualizando

--- Projeto_9.1/src/pages/Layout.jsx
+++ Projeto_9.1/src/pages/Layout.jsx
@@ -1,25 +1,35 @@
 import { Outlet, Link } from "react-router-dom";
+import '../index.css';
 
 const Layout = () => {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <div className="container">
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
           </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
+          <li className="nav-item">
+            <Link to="/blogs" className="nav-link">Blogs</Link>
           </li>
-          <li>
-            <Link to="/contact">Contact</Link>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">Contact</Link>
           </li>
         </ul>
       </nav>
 
-      <Outlet />
-    </>
-  )
+      <main>
+        <Outlet />
+        <p>- mae tem café?</p>
+        <p>- tem não</p>
+        <p>- O vazio dentro de mim é como uma xícara de café, esquecida no canto de uma mesa. o aroma que um dia foi reconfortante, agora se dispersa no ar, deixando apenas o amargor de uma bebida que que esfriou com o tempo. Cada gole me lembra da ausência de calor, da solidão que invade o corpo e a alma.</p>
+      </main>
+
+      <footer>
+        <p>&copy; 2024 FDS</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
